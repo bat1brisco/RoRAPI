5a3d480b08b98a16b15ff984b28c55ec5bf5ae0d
Brisco <brisco.go2designlab@gmail.com>
2019-08-07 12:28:36 +0800

    API FOR POST

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,62 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def login
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      user_details = { email: user.email, contact: user.contact, address: user.address }
+      token = JWT.encode user_details, nil, 'none'
+      render json: {status: 'REQUEST SUCCESS', message: 'Successfully Loggedin', data: token}, status: :ok
+    else
+      render json: {status: 'REQUEST SUCCESS', message: 'Invalid Email or Password', data: {}}, status: :ok
+    end
+  end
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :address, :contact)
+      
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  has_secure_password
+end
